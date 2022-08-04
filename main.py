35e5578b1877a23440024fd7980d9848f5253833
Shubham Kumar Singh <denyshubham@gmail.com>
2022-08-04 06:55:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 '''
 Sample Flask Auth App
 '''
-from flask import Flask
+
+from flask import Flask, request
 from flask_api import status
-from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required,JWTManager
+from flask_jwt_extended import create_access_token,get_jwt_identity,verify_jwt_in_request,JWTManager
 import os
 
 class Users():
@@ -15,33 +16,36 @@ class Users():
     def to_dict(self):
         return {'UserID': self.userId,'FullName': self.userFullName,'Email': self.userEmail}
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=False)
 app.config['JWT_SECRET_KEY'] = os.getenv('APP_JWT_SECRET', os.urandom(32))
 jwt = JWTManager(app)
 
-@app.route('/healthz')
+@app.route('/v1/user', methods=['GET'])
+def user():
+    try:
+        verify_jwt_in_request()
+        if get_jwt_identity():
+            userId, userFullName, userEmail = get_jwt_identity()
+            user = Users(userId, userFullName, userEmail)
+            return user.to_dict(), status.HTTP_200_OK
+            
+    except Exception as e:
+        print(e)
+    return 'Invalid Token!', status.HTTP_400_BAD_REQUEST
+
+@app.route("/healtz", methods=['GET'])
 def healthz():
     return 'ok', status.HTTP_204_NO_CONTENT
 
-@app.route('/v1/user')
-@jwt_required()
-def user():
-    print(get_jwt_identity())
-    if get_jwt_identity():
-        userId, userFullName, userEmail = get_jwt_identity()
-        user = Users(userId, userFullName, userEmail)
-        return user.to_dict(), status.HTTP_200_OK
-    else:
-        return 'Invalid Token!', status.HTTP_400_BAD_REQUEST
-
-
-# def generate_temp_auth_token():
-#     user = Users('1', 'Shubham Kumar Singh', 'denyshubham@gmail.com')
-#     auth_token = create_access_token(identity=[user.userId, user.userFullName, user.userEmail], expires_delta=False)  # jason web token  
-#     return auth_token
+@app.route("/token", methods=['POST'])
+def generate_auth_token():
+    data = request.get_json()
+    user = Users(data.get('userId'), data.get('userFullName'), data.get('userEmail'))
+    auth_token = create_access_token(identity=[user.userId, user.userFullName, user.userEmail], expires_delta=False)  # jason web token  
+    return auth_token
   
 if __name__=='__main__':
   jwtSampleToken = os.getenv('APP_JWT_SECRET', None)
   if jwtSampleToken is None:
     print("APP_JWT_SECRET environment variable not set")
-  app.run(host="0.0.0.0", port=3000, debug=True)
+  app.run(host="0.0.0.0", port=3000, debug=False)
